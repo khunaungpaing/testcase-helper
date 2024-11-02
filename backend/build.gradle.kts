26a560e0325a12ac9ce937a0594cd343a60c09d2
khunaungpaing <khunaung551p@prestigein.com>
2024-11-02 13:46:27 +0630

    add swagger configuration.

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -25,6 +25,7 @@ repositories {
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation ("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.6.0")
     implementation("org.springframework.boot:spring-boot-starter-web")
     compileOnly("org.projectlombok:lombok")
     developmentOnly("org.springframework.boot:spring-boot-docker-compose")
---
+++ backend/src/main/java/com/khun/testcasehelper/config/OpenApiConfig.java
@@ -0,0 +1,45 @@
+package com.khun.testcasehelper.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.servers.Server;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Testcase Helper API",
+                description = "API documentation for TestcaseHelper",
+                version = "1.0.0",
+                contact = @Contact(
+                        name = "Developer Team",
+                        url = "https://contact.testcasehelper.com",
+                        email = "email@testcasehelper.com"
+                ),
+                termsOfService = "Term of service"
+        ),
+        servers = {
+                @Server(
+                        description = "local ENV",
+                        url = "http://localhost:8080"
+                ),
+                @Server(
+                        description = "PROD ENV",
+                        url = "https://testcasehelper.org"
+                )
+        }
+)
+// the following is for security
+/*@SecurityScheme(
+        name = "bearerAuth",
+        description = "add description",
+        scheme = "bearer",
+        type = SecuritySchemeType.HTTP,
+        bearerFormat = "JWT",
+        in = SecuritySchemeIn.HEADER
+)*/
+public class OpenApiConfig {
+
+}
+
